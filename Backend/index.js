66a95b440f39fae296f687f6f5d0a646ca27163a
Simon Tamene <128576645+ST-dev100@users.noreply.github.com>
2024-04-04 05:28:06 -0700

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,18 +1,13 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const app = express();
-mongoose.connect('mongodb+srv://sima:1W238DFMvSh50S8M@cluster0.rz199gl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
-   
-const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log('Connected to MongoDB');
+// Define a route
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
-app.get('/users', (req, res) => {
-    res.json({a:"hvhv"}) 
-  });
-  
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
